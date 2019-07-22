b342d1dc0d77c85ac745cf4116470d4185afd18d
chris <ruehschr@gmail.com>
2019-07-21 22:15:09 -0700

    Sets default state in create spinner and initial view for CreateCount

--- app/src/main/java/com/rupert/thelaboratory/Activities/MeasureActivity.java
+++ app/src/main/java/com/rupert/thelaboratory/Activities/MeasureActivity.java
@@ -3,6 +3,8 @@ package com.rupert.thelaboratory.Activities;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
@@ -13,6 +15,7 @@ import com.rupert.thelaboratory.Measurement.Date;
 public class MeasureActivity extends AppCompatActivity {
 
     private String[] measurementTypes = {Count.Name(), Date.Name()};
+    private View createCountView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +24,46 @@ public class MeasureActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        createCountView = findViewById(R.id.count_add);
+
         Spinner spinner = (Spinner) findViewById(R.id.measurement_type_spinner);
         ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
-                android.R.layout.simple_spinner_item, measurementTypes);
+                android.R.layout.simple_spinner_item);
+        adapter.add("NONE");
+        adapter.addAll(measurementTypes);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position == 0) {
+                    hideAll();
+                    return;
+                }
+                String measurementName = measurementTypes[position-1];
+                if (measurementName.equals(Count.Name())) {
+                    showCount();
+                } else if (measurementName.equals(Date.Name())) {
+                    showDate();
+                }
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                hideAll();
+            }
+        });
+    }
+
+    private void hideAll() {
+        createCountView.setVisibility(View.INVISIBLE);
+    }
+
+    private void showCount() {
+        createCountView.setVisibility(View.VISIBLE);
+    }
+
+    private void showDate() {
 
     }
 
