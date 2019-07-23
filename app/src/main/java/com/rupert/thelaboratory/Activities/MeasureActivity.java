3253ec0ed55d6a61f3dd3adc46725df0a0cd6b6a
chris <ruehschr@gmail.com>
2019-07-22 23:39:14 -0700

    Inflates measure creation views instead of hiding

--- app/src/main/java/com/rupert/thelaboratory/Activities/MeasureActivity.java
+++ app/src/main/java/com/rupert/thelaboratory/Activities/MeasureActivity.java
@@ -1,11 +1,14 @@
 package com.rupert.thelaboratory.Activities;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 import com.rupert.thelaboratory.R;
@@ -15,7 +18,9 @@ import com.rupert.thelaboratory.Measurement.Date;
 public class MeasureActivity extends AppCompatActivity {
 
     private String[] measurementTypes = {Count.Name(), Date.Name()};
-    private View createCountView;
+    //private View createCountView;
+    private LinearLayout measureBucket;
+    private LayoutInflater inflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +29,23 @@ public class MeasureActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        createCountView = findViewById(R.id.count_add);
+        //createCountView = findViewById(R.id.count_add);
+        measureBucket = findViewById(R.id.measure_bucket);
+        inflater = (LayoutInflater) getApplicationContext().getSystemService(
+                Context.LAYOUT_INFLATER_SERVICE);
 
         Spinner spinner = (Spinner) findViewById(R.id.measurement_type_spinner);
-        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
+        ArrayAdapter<CharSequence> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_spinner_item);
-        adapter.add("NONE");
+        adapter.add("    ");
         adapter.addAll(measurementTypes);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                hideAll();
                 if (position == 0) {
-                    hideAll();
                     return;
                 }
                 String measurementName = measurementTypes[position-1];
@@ -56,15 +64,17 @@ public class MeasureActivity extends AppCompatActivity {
     }
 
     private void hideAll() {
-        createCountView.setVisibility(View.INVISIBLE);
+        measureBucket.removeAllViews();
     }
 
     private void showCount() {
-        createCountView.setVisibility(View.VISIBLE);
+        View v = inflater.inflate(R.layout.create_count, null);
+        measureBucket.addView(v);
     }
 
     private void showDate() {
-
+        View v = inflater.inflate(R.layout.create_date, null);
+        measureBucket.addView(v);
     }
 
 }
